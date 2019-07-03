394457981b69969ad8812558a180ab7cf292f165
rigor789 <rigor789@gmail.com>
2019-07-03 23:10:45 +0200

    Initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <Page>
+        <ActionBar title="WebView YouTube!"/>
+        <GridLayout @loaded="setupWebviewInterface">
+            <WebView ref="webView"/>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+  import {WebViewInterface} from 'nativescript-webview-interface'
+
+  export default {
+    methods: {
+      setupWebviewInterface() {
+        console.log('setting up WW interface')
+        this.oWebViewInterface = new WebViewInterface(this.$refs.webView.nativeView, '~/www/index.html')
+
+        this.oWebViewInterface.on('playerStateChanged', (data) => {
+          console.log(`playerStateChanged`)
+          console.log(data)
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
